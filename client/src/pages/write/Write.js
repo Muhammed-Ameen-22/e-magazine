53e8ac6fa7cb1d06aeaa363b8d462188bed30385
Muhammed-Ameen-22 <muhammedameen2222@gmail.com>
2022-02-20 12:36:13 +0530

    Resetting write file

--- client/src/pages/write/Write.js
+++ client/src/pages/write/Write.js
@@ -11,80 +11,142 @@ export default function Write() {
   const [file, setFile] = useState(null);
   const { user } = useContext(Context);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setError(false);
-    // const newPost = {
-    //   username: user.username,
-    //   title,
-    //   desc,
-    try {
-    const res = await axios.post(process.env.REACT_APP_SERVER_URL+"/createPost", {
-      image,
-      title,
-      desc
-    });  
-    console.log("RES"+res);
-  }
-  catch (err) {
-   setError(true);
- }
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     setError(false);
+//     // const newPost = {
+//     //   username: user.username,
+//     //   title,
+//     //   desc,
+//     try {
+//     const res = await axios.post(process.env.REACT_APP_SERVER_URL+"/createPost", {
+//       image,
+//       title,
+//       desc
+//     });  
+//     console.log("RES"+res);
+//   }
+//   catch (err) {
+//    setError(true);
+//  }
 
-  // };
-    // if (file) {
-    //   const data =new FormData();
-    //   const filename = Date.now() + file.name;
-    //   data.append("name", filename);
-    //   data.append("file", file);
-    //   newPost.photo = filename;
-    //   try {
-    //     await axios.post("/upload", data);
-    //   } catch (err) {}
-    // }
-    // try {
-    //   const res = await axios.post("/posts", newPost);
-    //   window.location.replace("/post/" + res.data._id);
-    // } catch (err) {}
+//   // };
+//     // if (file) {
+//     //   const data =new FormData();
+//     //   const filename = Date.now() + file.name;
+//     //   data.append("name", filename);
+//     //   data.append("file", file);
+//     //   newPost.photo = filename;
+//     //   try {
+//     //     await axios.post("/upload", data);
+//     //   } catch (err) {}
+//     // }
+//     // try {
+//     //   const res = await axios.post("/posts", newPost);
+//     //   window.location.replace("/post/" + res.data._id);
+//     // } catch (err) {}
+//   };
+//   return (
+//     <div className="write">
+//       {file && (
+//         <img className="writeImg" src={URL.createObjectURL(file)} alt="" />
+//       )}
+//       <form className="writeForm" onSubmit={handleSubmit}>
+//         <div className="writeFormGroup">
+//           <label htmlFor="fileInput">
+//             <i className="writeIcon fas fa-plus"></i>
+//           </label>
+//           <input
+//             type="file"
+//             name='image'
+//             id="fileInput"
+//             // style={{ display: "none" }}
+//             onChange={(e) => setImage(e.target.files[0])}
+//           />
+//           <input
+//             type="text"
+//             name= 'title'
+//             placeholder="Title"
+//             className="writeInput"
+//             autoFocus={true}
+//             onChange={e=>setTitle(e.target.value)}
+//           />
+//         </div>
+//         <div className="writeFormGroup">
+//           <textarea
+//             placeholder="Tell your story..."
+//             type="text"
+//             name='desc'
+//             className="writeInput writeText"
+//             onChange={e=>setDesc(e.target.value)}
+//           ></textarea>
+//         </div>
+//         <button className="writeSubmit" type="submit" onClick={handleSubmit}>
+//           Publish
+//         </button>
+//       </form>
+//     </div>
+//   );
+// }
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  const newPost = {
+    username: user.username,
+    title,
+    desc,
   };
-  return (
-    <div className="write">
-      {file && (
-        <img className="writeImg" src={URL.createObjectURL(file)} alt="" />
-      )}
-      <form className="writeForm" onSubmit={handleSubmit}>
-        <div className="writeFormGroup">
-          <label htmlFor="fileInput">
-            <i className="writeIcon fas fa-plus"></i>
-          </label>
-          <input
-            type="file"
-            name='image'
-            id="fileInput"
-            // style={{ display: "none" }}
-            onChange={(e) => setImage(e.target.files[0])}
-          />
-          <input
-            type="text"
-            name= 'title'
-            placeholder="Title"
-            className="writeInput"
-            autoFocus={true}
-            onChange={e=>setTitle(e.target.value)}
-          />
-        </div>
-        <div className="writeFormGroup">
-          <textarea
-            placeholder="Tell your story..."
-            type="text"
-            name='desc'
-            className="writeInput writeText"
-            onChange={e=>setDesc(e.target.value)}
-          ></textarea>
-        </div>
-        <button className="writeSubmit" type="submit" onClick={handleSubmit}>
-          Publish
-        </button>
-      </form>
-    </div>
-  );
+  if (file) {
+    const data =new FormData();
+    const filename = Date.now() + file.name;
+    data.append("name", filename);
+    data.append("file", file);
+    newPost.photo = filename;
+    try {
+      await axios.post("/upload", data);
+    } catch (err) {}
+  }
+  try {
+    const res = await axios.post("/posts", newPost);
+    window.location.replace("/post/" + res.data._id);
+  } catch (err) {}
+};
+return (
+  <div className="write">
+    {file && (
+      <img className="writeImg" src={URL.createObjectURL(file)} alt="" />
+    )}
+    <form className="writeForm" onSubmit={handleSubmit}>
+      <div className="writeFormGroup">
+        <label htmlFor="fileInput">
+          <i className="writeIcon fas fa-plus"></i>
+        </label>
+        <input
+          type="file"
+          id="fileInput"
+          style={{ display: "none" }}
+          onChange={(e) => setFile(e.target.files[0])}
+        />
+        <input
+          type="text"
+          placeholder="Title"
+          className="writeInput"
+          autoFocus={true}
+          onChange={e=>setTitle(e.target.value)}
+        />
+      </div>
+      <div className="writeFormGroup">
+        <textarea
+          placeholder="Tell your story..."
+          type="text"
+          className="writeInput writeText"
+          onChange={e=>setDesc(e.target.value)}
+        ></textarea>
+      </div>
+      <button className="writeSubmit" type="submit">
+        Publish
+      </button>
+    </form>
+  </div>
+);
 }
